f08d646bfa4b281ea62e55197d6b43ae1383252e
Sergey Burashnikov <burashnikov.sergei@mail.ru>
2025-12-03 16:07:52 +0300

    Fix docker

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,18 @@
-FROM openjdk:21-jdk-slim
+FROM gradle:8-jdk21 AS builder
+
+WORKDIR /app
+COPY app/ .
+
+RUN gradle clean build --no-daemon
+
+FROM eclipse-temurin:21-jdk
 
 WORKDIR /app
 
-COPY build/libs/itrum-0.0.1.jar app.jar
+COPY --from=builder /app/build/libs/*.jar app.jar
 
 RUN apt-get update && apt-get install -y curl
+
 HEALTHCHECK --interval=7s --timeout=5s --retries=10 CMD curl -k http://localhost:8800/actuator/health | grep -q 'UP' || exit 1
 
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.5.0")
     implementation("org.liquibase:liquibase-core:4.28.0")
     implementation("org.postgresql:postgresql:42.7.3")
-    implementation("net.lbruun.springboot:preliquibase-spring-boot-starter:1.6.1")
     implementation("org.springframework.boot:spring-boot-starter-actuator:3.5.0")
     implementation("org.springframework.boot:spring-boot-starter-web:3.5.0")
     implementation("org.projectlombok:lombok:1.18.36")
